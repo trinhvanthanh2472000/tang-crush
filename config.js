eb7c23e46c8986cb1614c64180824e73620f1479
trinhvanthanh2472000 <85282841+trinhvanthanh2472000@users.noreply.github.com>
2021-06-03 16:37:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,19 @@
 const CONFIG = {
     introTitle: 'Bae à!',
-    introDesc: `Trang ưiii
-   trò chơi này mình giành riêng cho Trangg
-    gửi đến Trang 1 cô nàng xinh xắn`,
+    introDesc: `cái này là giành riêng cho Trang
+    gửi đến em 1 cô nàng xinh xắn 
+    được thưn Trang là điều may mắn
+    đến bao giờ mình mới có nhau`,
     btnIntro: 'hihi',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Có thưn ngta hong dí vào đây là iu đó nha <33',
-    btnNo: 'Không, chả thèm thương douu :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao lại iu mình <3',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'có thưn mình hong dí vào là phải thương đó <33',
+    btnNo: 'Không, chả thương tẹo nào cả:3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao lại yêu mình  <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'hiaaaa thế thì hết dịch hẹn yêu thuii',
+    messDesc: 'vậy thì hết dịch chúng mình hẹn yêu.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100006753014781' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://trinhvanthanh2472000.github.io/tang-crush/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
