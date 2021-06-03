bb1b51c6802cf59ed4421126cfa31beea6abf8e2
trinhvanthanh2472000 <85282841+trinhvanthanh2472000@users.noreply.github.com>
2021-06-03 18:38:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,14 +7,14 @@ const CONFIG = {
     Đến bao giờ mình mới có nhau`,
     btnIntro: 'hihi',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'có thưn mình hong dí vào là phải thương đó <33',
+    desc: 'Thương tui hong😘 không yêu anh cũng bỏ bùa cho yêu ',
+    btnYes: 'dí vào là phải thương đó <33',
     btnNo: 'Không, chả thương tẹo nào cả:3',
     question:'Trên thế giới hơn 7 tỉ người mà sao lại yêu mình  <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Mãi bên nhau bạn nhó 😘😘',
-    messDesc: 'hết dịch chúng mình hẹn yêu.',
+    messDesc: 'thương em rất nhiềuuuuuuuu.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100006753014781' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
