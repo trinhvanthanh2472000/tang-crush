e1e8edd458ae7a6c73deff6b9ae0381543d9a117
trinhvanthanh2472000 <85282841+trinhvanthanh2472000@users.noreply.github.com>
2021-06-03 17:46:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,8 +13,8 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao lại yêu mình  <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'vậy thì hết dịch chúng mình hẹn yêu.',
+    mess: 'Mình biết mà 🥰. Mãi bên nhau bạn nhó 😘😘',
+    messDesc: 'hết dịch chúng mình hẹn yêu.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://trinhvanthanh2472000.github.io/tang-crush/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
